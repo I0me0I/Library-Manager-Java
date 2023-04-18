1f6d4fe00cd85ee56a04f78cf639ffa873a13879
甘立煌 <qq1797767@outlook.com>
2023-04-18 17:12:23 +0800

    持续更新中

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
-        Administrator admin = new Administrator();
-        admin.menu();
+        while(true){
+            System.out.print(
+                "\n欢迎来到Java图书馆管理系统！\n\n"
+                + "1.进入图书管理子系统\n"
+                + "2.进入图书借阅子系统\n"
+                + "0.退出\n\n"
+                + "请选择："
+            );
+    
+            Scanner scan = new Scanner(System.in);
+            if(!scan.hasNextInt()){
+                System.out.println("请输入数字！");
+                continue;
+            }
+            int key = scan.nextInt();
+
+            switch(key){
+                case 1:
+                    new Administrator().menu();
+                    return;
+                
+                case 2:
+                    new GeneralUser().menu();
+                    return;
+
+                case 0:
+                    return;
+                
+                default:
+                    System.out.println("无效的数字，请重新输入！");
+            }
+        }
     }
 }
